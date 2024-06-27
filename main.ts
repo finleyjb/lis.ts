8fc7d10b595443b00931d842f790245fc1b851b5
Finley Baker <finjothegreat@gmail.com>
2024-06-27 08:56:58 +0200

    Refine console.log statements

--- main.ts
+++ main.ts
@@ -5,6 +5,8 @@ import { Heap } from "./heap.ts";
 const heap = new Heap();
 if (import.meta.main) {
   const exp = parse(tokenize("(if (== 1 1) 1 1.1)"));
+  console.log("\n=== Executing expression ===");
   console.log(exp);
+  console.log('\n=== Result ===')
   console.log(exec(exp));
 }
